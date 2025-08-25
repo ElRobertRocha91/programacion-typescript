50905b9fc3ee6e1c8dc0cffc6e29a81a3018dda9
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-08-24 21:01:08 -0300

    Agregando ejemplos de variables basicas

---
+++ Variables_base/main.js
@@ -0,0 +1,14 @@
+// Variable de tipo string (cadena)
+var nombre = "Admin";
+// Variable de tipo number (número)
+var edad = 39;
+// Variable de tipo booleano (verdadero o falso)
+var esDesarrolladorWeb = true;
+// Variables que puede ser undefined o string
+var mes;
+// Variable que inicializa como null
+var variableNull = null;
+console.log("Mes inicial:", mes);
+mes = "Enero";
+console.log("Mes actualizado:", mes);
+console.log("Hola ".concat(nombre));
---
+++ Variables_base/main.ts
@@ -0,0 +1,21 @@
+// Variable de tipo string (cadena)
+var nombre: string = "Admin";
+
+// Variable de tipo number (número)
+var edad: number = 39;
+
+// Variable de tipo booleano (verdadero o falso)
+var esDesarrolladorWeb: boolean = true;
+
+// Variables que puede ser undefined o string
+var mes: string | undefined;
+
+// Variable que inicializa como null
+var variableNull: null = null;
+
+console.log("Mes inicial:", mes);
+
+mes = "Enero";
+console.log("Mes actualizado:", mes);
+
+console.log(`Hola ${nombre}`);
